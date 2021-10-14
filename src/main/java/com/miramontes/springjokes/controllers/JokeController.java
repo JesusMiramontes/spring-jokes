6d4b8598d07439bc1d2d959926426f773e2f00ec
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-10-14 15:49:53 -0500

    Created jokes controller

---
+++ src/main/java/com/miramontes/springjokes/controllers/JokeController.java
@@ -0,0 +1,21 @@
+package com.miramontes.springjokes.controllers;
+
+import com.miramontes.springjokes.services.JokesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private final JokesService jokesService;
+
+    public JokeController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokesService.getJoke());
+        return "index";
+    }
+}
